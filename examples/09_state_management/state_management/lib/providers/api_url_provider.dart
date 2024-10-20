8261637f5ffd1490bc8e0e888a5831e45e9d99a6
ae <erradi@live.com>
2024-10-20 10:02:29 +0300

    Updated state management slides

--- examples/09_state_management/state_management/lib/providers/api_url_provider.dart
+++ examples/09_state_management/state_management/lib/providers/api_url_provider.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 // Defining a Provider for the API URL
-final apiUrlProvider = Provider<String>((ref) {
-  return "https://api.example.com";
-});
+final apiUrlProvider = Provider<String>(
+  (ref) => "https://api.example.com"
+);
--- examples/09_state_management/state_management/lib/providers/counter_provider.dart
+++ examples/09_state_management/state_management/lib/providers/counter_provider.dart
@@ -21,6 +21,14 @@ class CounterNotifier extends Notifier<int> {
 /*
   NotifierProvider creates an instance of CounterNotifier, 
   allowing other widgets to listen for state changes.
+
+  The NotifierProvider<CounterNotifier, int> has two generic data types:
+  - CounterNotifier (The Notifier class): specifies the type of 
+    the Notifier class that will manage the state. 
+    The Notifier is responsible for managing how the state is updated.
+  - int (The type of state being managed): specifies the type of the 
+  state being managed by the Notifier. 
+  The state is what the UI listens to and rebuilds when it changes.
 */
 final counterProvider =
     NotifierProvider<CounterNotifier, int>(() => CounterNotifier());
